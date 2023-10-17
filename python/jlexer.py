3cc4a3dd153d67b505c0d2ae665dec41d328fc14
0racle <joshuamy@gmail.com>
2023-10-18 08:52:21 +1100

    Reformat, and also fix bad unpack code
    The original pattern that detects unpacking syntax was fixed some time
    ago to prevent catastrophic backtracking. I only just realised that
    pattern was still in use for unpacking inside a verb definition.
    Unfortunately this syntax will no highlight pronouns/proverbs inside the
    unpack.
    
    Also reformated with black.

--- python/jlexer.py
+++ python/jlexer.py
@@ -1,163 +1,172 @@
 #!/usr/bin/env python3
 
 from pygments.lexer import RegexLexer, include, bygroups
-from pygments.token import *
+from pygments.token import Token
 
-Verb = Operator
-Adverb = Keyword.Type
-Conjunction = Keyword
-Punct = Punctuation
-Noun = Name
-RankConst = Name.Tag
-Control = Name.Label
-DefHeader = Name.Function
-ExplicitDef = Name.Decorator
-DirectDef = Name.Decorator
-Def = Name.Decorator
-Copula = Generic
+Verb = Token.Operator
+Adverb = Token.Keyword.Type
+Conjunction = Token.Keyword
+Noun = Token.Name
+RankConst = Token.Name.Tag
+Control = Token.Name.Label
+DefHeader = Token.Name.Function
+ExplicitDef = Token.Name.Decorator
+DirectDef = Token.Name.Decorator
+Def = Token.Name.Variable
+Copula = Token.Generic
 
 bdef_noun_multi = r"(\(+\s*)*(noun|0)\)*\s+(\(*define\)*|(def|:)\s*\(*0\)*)"
 qdef_verb_inline = r"(adverb|conjunction|verb|monad|dyad|[1-4]|13)\s+(def|:)\s*'"
-bdef_verb_multi  = r"(\(+\s*)*(adverb|conjunction|verb|monad|dyad|[1-4]|13)\)*\s+(\(*define\)*|(def|:)\s*\(*0\)*)\s*"
+bdef_verb_multi = r"(\(+\s*)*(adverb|conjunction|verb|monad|dyad|[1-4]|13)\)*\s+(\(*define\)*|(def|:)\s*\(*0\)*)\s*"
 bqdef_verb_inline = r"(adverb|conjunction|verb|monad|dyad|[1-4]|13)\s+(def|:)\s*\('"
 
+
 class CustomLexer(RegexLexer):
-    name = 'J'
-    aliases = ['J']
-    filenames = ['*.ijs']
+    name = "J"
+    aliases = ["J"]
+    filenames = ["*.ijs"]
 
     tokens = {
-        'root': [
-            # (r"#!.*", Comment.Hashbang), 
-            include('expdef'),
-            include('syntax'),
-            include('literal'),
-            include('string'),
-            (r"\{\{", DirectDef, 'ddef'), 
+        "root": [
+            # (r"#!.*", Comment.Hashbang),
+            include("expdef"),
+            include("syntax"),
+            include("literal"),
+            include("string"),
+            (r"\{\{", DirectDef, "ddef"),
         ],
-        'literal': [
-            (r"\b[A-Za-z][A-Za-z0-9_]*\b", Literal),
+        "literal": [
+            (r"\b[A-Za-z][A-Za-z0-9_]*\b", Token.Literal),
         ],
-        'syntax': [
-            (r"=[.:']", Copula), 
-            include('unpack'),
-            include('comment'),
-            include('conjunction'),
-            include('verb'),
-            include('adverb'),
-            include('noun'),
-            include('rank'),
-            include('number'),
-            include('punct'),
-            include('whitespace'),
+        "syntax": [
+            (r"=[.:']", Copula),
+            include("unpack"),
+            include("comment"),
+            include("conjunction"),
+            include("verb"),
+            include("adverb"),
+            include("noun"),
+            include("rank"),
+            include("number"),
+            include("punct"),
+            include("whitespace"),
         ],
-        'punct': [
-            (r"\(|(?<=.)\)", Punctuation), 
+        "punct": [
+            (r"\(|(?<=.)\)", Token.Punctuation),
         ],
-        'whitespace' : [
-            (r"\s+", Whitespace),
+        "whitespace": [
+            (r"\s+", Token.Whitespace),
         ],
-        'unpack': [
-            (r"(')(`?)([\w\s]*)(')(\s*=[.:])",
-                bygroups(Def, Conjunction, Literal, Def, Copula),
+        "unpack": [
+            (
+                r"(')(`?)([\w\s]*)(')(\s*=[.:])",
+                bygroups(
+                    Token.Name.Function,
+                    Conjunction,
+                    Token.Literal,
+                    Token.Name.Function,
+                    Copula,
+                ),
             )
         ],
-        'unpack_in_def': [
-            (r"'(?=(`?\s*[a-zA-Z][a-zA-Z0-9_]*)+\s*'\s*=[.:])",
-                Name.Function, 'unpack_assign_in_def'),
+        "unpack_in_def": [
+            # Doesn't highlight pronoun/proverbs correctly
+            (
+                r"(')(`?)([\w\s]*)(')(\s*=[.:])",
+                bygroups(
+                    Token.Name.Function,
+                    Conjunction,
+                    Token.Literal,
+                    Token.Name.Function,
+                    Copula,
+                ),
+            )
         ],
-        'unpack_assign_in_def': [
-            (r"'", Name.Function),
-            (r"`", Conjunction), 
-            include('args'),
-            include('literal'),
-            include('whitespace'),
-            (r"=[.:']", Copula, '#pop'), 
+        "string": [(r"'[^']*'", Token.String)],
+        "nstring": [(r"''.*?''", Token.String)],
+        "expdef": [
+            include("bdef_noun"),
+            include("qdef_verb"),
+            include("bdef_verb"),
+            include("bqdef_verb"),
         ],
-        'string': [ (r"'[^']*'", String) ],
-        'nstring': [ (r"''.*?''", String) ],
-        'expdef': [
-            include('bdef_noun'),
-            include('qdef_verb'),
-            include('bdef_verb'),
-            include('bqdef_verb'),
+        "bdef_noun": [
+            (bdef_noun_multi, ExplicitDef, "bdef_noun_multi"),
+            (r"^\)", ExplicitDef),
         ],
-        'bdef_noun' : [
-            (bdef_noun_multi, ExplicitDef,
-                'bdef_noun_multi'),
-            (r"^\)", ExplicitDef), 
+        "bdef_noun_multi": [
+            (r"(?s).*?(?=^\))", Token.String, "#pop"),
         ],
-        'bdef_noun_multi': [
-            (r"(?s).*?(?=^\))", String, '#pop'),
+        "bdef_verb": [
+            (bdef_verb_multi, ExplicitDef, "bdef_verb_multi"),
         ],
-        'bdef_verb': [
-            (bdef_verb_multi, ExplicitDef,
-                'bdef_verb_multi'),
+        "bdef_verb_multi": [
+            include("control"),
+            include("expdef"),
+            include("syntax"),
+            include("args"),
+            include("string"),
+            include("literal"),
+            (r"^\)", ExplicitDef, "#pop"),
         ],
-        'bdef_verb_multi': [
-            include('control'),
-            include('expdef'),
-            include('syntax'),
-            include('args'),
-            include('string'),
-            include('literal'),
-            (r"^\)", ExplicitDef, '#pop'),
+        "qdef_verb": [
+            (qdef_verb_inline, ExplicitDef, "qdef_verb_inline"),
         ],
-        'qdef_verb': [
-            (qdef_verb_inline, ExplicitDef,
-                'qdef_verb_inline'),
+        "qdef_verb_inline": [
+            include("control"),
+            include("syntax"),
+            include("args"),
+            include("nstring"),
+            include("literal"),
+            (r"'", ExplicitDef, "#pop"),
         ],
-        'qdef_verb_inline': [
-            include('control'),
-            include('syntax'),
-            include('args'),
-            include('nstring'),
-            include('literal'),
-            (r"'", ExplicitDef, '#pop'),
+        "bqdef_verb": [
+            (bqdef_verb_inline, ExplicitDef, "bqdef_verb_inline"),
         ],
-        'bqdef_verb': [
-            (bqdef_verb_inline, ExplicitDef,
-                'bqdef_verb_inline'),
-        ],
-        'bqdef_verb_inline': [
-            include('control'),
-            include('syntax'),
-            include('args'),
-            include('nstring'),
-            include('literal'),
-            (r"'\)", ExplicitDef, '#pop'),
+        "bqdef_verb_inline": [
+            include("control"),
+            include("syntax"),
+            include("args"),
+            include("nstring"),
+            include("literal"),
+            (r"'\)", ExplicitDef, "#pop"),
             (r"'", ExplicitDef),
         ],
-        'comment': [
-            (r"NB\.(?![.:]).*", Comment), 
+        "comment": [
+            (r"NB\.(?![.:]).*", Token.Comment),
         ],
-        'args': [
+        "args": [
             (r"[mnxy](?!\w)", Noun),
             (r"[uv](?!\w)", Verb),
         ],
-        'ddef': [
+        "args_in_unpack": [
+            (r"[mnxy](?!\w)", Noun),
+            (r"[uv](?!\w)", Verb),
+            (r"\b[A-Za-z][A-Za-z0-9_]*\b", Token.Literal),
+        ],
+        "ddef": [
             (r"\)[mdvac*]", DefHeader),
-            (r"\)n", DefHeader, 'directstring'),
-            (r"\}\}", DirectDef, '#pop'), 
-            (r"\{\{", DirectDef, '#push'), 
-            include('unpack_in_def'),
-            include('control'),
-            include('args'),
-            include('syntax'),
-            include('string'),
-            include('literal'),
+            (r"\)n", DefHeader, "directstring"),
+            (r"\}\}", DirectDef, "#pop"),
+            (r"\{\{", DirectDef, "#push"),
+            include("unpack_in_def"),
+            include("control"),
+            include("args"),
+            include("syntax"),
+            include("string"),
+            include("literal"),
         ],
-        'directstring': [
-            (r"\}\}", DirectDef), 
-            (r"(?s).*?(?=\}\})", String, '#pop'),
+        "directstring": [
+            (r"\}\}", DirectDef),
+            (r"(?s).*?(?=\}\})", Token.String, "#pop"),
         ],
-        'verb': [
+        "verb": [
             (r"__?:", Verb),
             (r";:", Verb),
             (r"\{::", Verb),
             (r"[;=!]", Verb),
             (r"[-+*<>#$%|,][.:]?", Verb),
-            (r"(?<!\{)\{(?!\{)[.:]?", Verb), # single '{'
+            (r"(?<!\{)\{(?!\{)[.:]?", Verb),  # single '{'
             (r"\}[.:]", Verb),
             (r"~[.:]", Verb),
             (r"\^\.?", Verb),
@@ -171,15 +180,15 @@ class CustomLexer(RegexLexer):
             (r"_?[0-9]:", Verb),
             (r'"[.:]', Verb),
         ],
-        'adverb': [
-            (r"(?<!\})\}(?!\})", Adverb), # single '}'
+        "adverb": [
+            (r"(?<!\})\}(?!\})", Adverb),  # single '}'
             (r"~", Adverb),
             (r"\/(\.\.?)?", Adverb),
             (r"\b[bfM]\.", Adverb),
             (r"\]:", Adverb),
             (r"\\\.?", Adverb),
         ],
-        'conjunction': [
+        "conjunction": [
             (r"\s+\.", Conjunction),
             (r"^\s*:", Conjunction),
             (r"\.$", Conjunction),
@@ -188,7 +197,7 @@ class CustomLexer(RegexLexer):
             (r"\^:", Conjunction),
             (r":[.:]?", Conjunction),
             (r"![.:]{1}", Conjunction),
-            (r"`[:]?", Conjunction), 
+            (r"`[:]?", Conjunction),
             (r"[@&][.:]?", Conjunction),
             (r"&\.:", Conjunction),
             (r"\b[dtmDH]\.", Conjunction),
@@ -196,27 +205,29 @@ class CustomLexer(RegexLexer):
             (r"\bF[.:][.:]?", Conjunction),
             (r"[\[\]]\.", Conjunction),
         ],
-        'rank': [
-            (r'(")((\s*_?([0-9]+x?)?){1,3})',
+        "rank": [
+            (
+                r'(")((\s*_?([0-9]+x?)?){1,3})',
                 bygroups(Conjunction, RankConst),
-            ), 
+            ),
         ],
-        'noun': [
+        "noun": [
             (r"a[.:]", Noun),
             (r"_\.", Noun),
             (r"_(?![:_\d])", Noun),
         ],
-        'number': [
-            (r"_?\.?\d+(?!:)[_\.0-9Ea-z]*", Number),
-            (r"_(?=_)", Number),
+        "number": [
+            (r"_?\.?\d+(?!:)[_\.0-9Ea-z]*", Token.Number),
+            (r"_(?=_)", Token.Number),
         ],
-        'control' : [
+        "control": [
             (r"\b(assert|break|case|catch|catchd)\.", Control),
             (r"\b(catcht|continue|do|else|elseif)\.", Control),
             (r"\b(end|fcase|for|if|return|select)\.", Control),
             (r"\b(throw|trap|try|while|whilst)\.", Control),
-            (r"\b(for_|goto_|label_)([^.]+)(\.)",
-                bygroups(Control, Literal, Control),
+            (
+                r"\b(for_|goto_|label_)([^.]+)(\.)",
+                bygroups(Control, Token.Literal, Control),
             ),
         ],
     }
